4e2289264c5f84ebeba9dcbc51f25fea21b4d5e4
Karsten Lang <karstenlang@gmail.com>
2013-03-13 21:15:46 +0100

    text counter finished (p.73)

--- src/dk/lang/android/yamba/StatusActivity.java
+++ src/dk/lang/android/yamba/StatusActivity.java
@@ -1,20 +1,28 @@
 package dk.lang.android.yamba;
 
 import winterwell.jtwitter.Twitter;
+import winterwell.jtwitter.TwitterException;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
+import android.graphics.Color;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class StatusActivity extends Activity implements OnClickListener {
+public class StatusActivity extends Activity implements OnClickListener, TextWatcher {
 	private static final String TAG = "StatusActivity";
 	EditText editText;
 	Button updateButton;
 	Twitter twitter;
+	TextView textCount;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -24,16 +32,51 @@ public class StatusActivity extends Activity implements OnClickListener {
 		// Find views
 		editText = (EditText) findViewById(R.id.editText);
 		updateButton = (Button) findViewById(R.id.buttonUpdate);
-		
 		updateButton.setOnClickListener(this);
+
+		textCount = (TextView) findViewById(R.id.textCount);
+		textCount.setText(Integer.toString(140));
+		textCount.setTextColor(Color.GREEN);
+		editText.addTextChangedListener(this);
 		
 		twitter = new Twitter("student", "password");
 		twitter.setAPIRootUrl("http://yamba.marakana.com/api");
 	}
 	
+	// Asynchronously post to twitter 
+	// (without this, we will get an android.os.NetworkOnMainThreadException)
+	// Gingerbread let you do networking on the UI threat, from Honeycomb this is no longer possible
+	class PostToTwitter extends AsyncTask<String, Integer, String> {
+		// Called to initiate the background activity
+		@Override
+		protected String doInBackground(String... statuses) {
+			try {
+				Twitter.Status status = twitter.updateStatus(statuses[0]);
+				return status.text;
+			} catch (TwitterException e) {
+				Log.e(TAG, e.toString());
+				e.printStackTrace();
+				return "Failed to post";
+			}
+		}
+		// Called when there is a status to be updated
+		@Override
+		protected void onProgressUpdate(Integer... values) {
+			super.onProgressUpdate(values);
+			// Not used in this case (but java demands that it is implemented anyway .. )
+		}
+		
+		// Called once the background activity has completed
+		@Override
+		protected void onPostExecute(String result) {
+			Toast.makeText(StatusActivity.this, result, Toast.LENGTH_LONG).show();
+		}
+		
+	}
 	// Called when button is clicked //
 	public void onClick(View v) {
-		twitter.setStatus(editText.getText().toString());
+		String status = editText.getText().toString();
+		new PostToTwitter().execute(status);
 		Log.d(TAG, "onClicked");
 	}
 	
@@ -43,5 +86,17 @@ public class StatusActivity extends Activity implements OnClickListener {
 		getMenuInflater().inflate(R.menu.status, menu);
 		return true;
 	}
-
+	
+	// TextWatcher methods
+	public void afterTextChanged(Editable statusText) {
+		int count = 140 - statusText.length();
+		textCount.setText(Integer.toString(count));
+		textCount.setTextColor(Color.GREEN);
+		if (count < 10) 
+			textCount.setTextColor(Color.YELLOW);
+		if (count < 0)
+			textCount.setTextColor(Color.RED);
+	}
+	public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+	public void onTextChanged(CharSequence s, int start, int before, int count) {}
 }
