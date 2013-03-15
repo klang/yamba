d20787dfcc82f7d6e9cfbd0653284f8bf1851c78
Karsten Lang <karstenlang@gmail.com>
2013-03-15 10:58:25 +0100

    Looping the Service (p.113)

--- src/dk/lang/android/yamba/UpdaterService.java
+++ src/dk/lang/android/yamba/UpdaterService.java
@@ -7,6 +7,11 @@ import android.util.Log;
 
 public class UpdaterService extends Service {
 	static final String TAG = "UpdaterService";
+
+	static final int DELAY = 60000;
+	private boolean runFlag = false;
+	private Updater updater;
+	
 	@Override
 	public IBinder onBind(Intent intent) {
 		return null;
@@ -15,12 +20,15 @@ public class UpdaterService extends Service {
 	@Override
 	public void onCreate() {
 		super.onCreate();
+		this.updater = new Updater();
 		Log.d(TAG, "onCreated");
 	}
 	
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
 		super.onStartCommand(intent, flags, startId);
+		this.runFlag=true;
+		this.updater.start();
 		Log.d(TAG, "onStarted");
 		return START_STICKY;
 	}
@@ -28,8 +36,34 @@ public class UpdaterService extends Service {
 	@Override
 	public void onDestroy() {
 		super.onDestroy();
+		this.runFlag=false;
+		this.updater.interrupt();
+		this.updater = null;
 		Log.d(TAG, "onDestroyed");
 	}
 	
-
+	/**
+	 * Threat that performs the actual update from the online service
+	 */
+	private class Updater extends Thread {
+		public Updater(){
+			super("UpdaterService-Updater");
+		}
+	
+	
+		@Override
+		public void run(){
+			UpdaterService updaterService = UpdaterService.this;
+			while (updaterService.runFlag) {
+				Log.d(TAG, "Updater running");
+				try {
+					// Some work goes here ...
+					Log.d(TAG, "Updater ran");
+					Thread.sleep(DELAY);
+				} catch (InterruptedException e) {
+					updaterService.runFlag = false;
+				}
+			}
+		}
+	} // Updater
 }
