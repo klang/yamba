72be565aaea4c3ed695b007810045b0f46ac091d
Karsten Lang <karstenlang@gmail.com>
2013-03-17 18:56:46 +0100

    BaseActivity in use, StatusData still is not .. a cluster fuck at the moment, but it works, after a fashion (p. 160)

--- src/dk/lang/android/yamba/TimelineActivity.java
+++ src/dk/lang/android/yamba/TimelineActivity.java
@@ -2,6 +2,7 @@ package dk.lang.android.yamba;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
@@ -11,13 +12,19 @@ import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 import android.widget.SimpleCursorAdapter.ViewBinder;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class TimelineActivity extends Activity {
+public class TimelineActivity extends BaseActivity {
 	DbHelper dbHelper; // this was deprecated in chapter 9 .. we are now in chapter 10
 	SQLiteDatabase db;
 	Cursor cursor;
 	ListView listTimeline;
-	TimelineAdapter adapter;
+
+	// wtf? disregarding the StatusData work again?!? (p.145)
+	static final String[] FROM = {DbHelper.C_CREATED_AT, DbHelper.C_USER, DbHelper.C_TEXT};
+	static final int[] TO = { R.id.textCreatedAt, R.id.textUser, R.id.textText };		
+
+	SimpleCursorAdapter adapter;
 	// View binder constant to inject business logic that converts a timestamp to relative time
 	static final ViewBinder VIEW_BINDER = new ViewBinder() {
 		public boolean setViewValue(View view, Cursor cursor, int columnIndex) {
@@ -36,9 +43,17 @@ public class TimelineActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.timeline);
 		
+		// Check if preferences have been set (this is not the right place to ask that question)
+		// .. yamba.getPrefs isn't implemented, of course .. 
+		/* TODO: resolve this cluster-fuck
+		if (yamba.getPrefs().getString("username", null) == null) {
+			startActivity(new Intent(this, PrefsActivity.class));
+			Toast.makeText(this, R.string.msgSetupPrefs, Toast.LENGTH_LONG).show();
+		}
+		*/
 		listTimeline = (ListView) findViewById(R.id.listTimeline);
 		
-		// Connect to database
+		// Connect to database TODO: maybe use the StatusData class to provide this?
 		dbHelper = new DbHelper(this);
 		db = dbHelper.getReadableDatabase();
 	}
@@ -47,17 +62,20 @@ public class TimelineActivity extends Activity {
 	@Override
 	public void onResume() {
 		super.onResume();
+		this.setupList();		
+	}
+	
+	private void setupList() {
 		// DbHelper.C_CREATED_AT + " Desc" == the private StatusData.GET_ALL_ORDER_BY
 		// besides, didn't we put all the functionality of the DbHelper into StatusData 
 		// to get rid of it?
 		// in fact, the cursor below is returned by:
 		//cursor = StatusData.getStatusUpdates();
-		
-		// Get the data from the database
+		// Get the data from the database TODO: get the database in the right way, via StatusData
 		cursor = db.query(DbHelper.TABLE,  null,  null,  null,  null,  null, DbHelper.C_CREATED_AT + " Desc");
 		startManagingCursor(cursor);
 		
-		adapter = new TimelineAdapter(this, cursor);
+		adapter = new SimpleCursorAdapter(this, R.layout.row, cursor, FROM, TO);
 		adapter.setViewBinder(VIEW_BINDER);
 		listTimeline.setAdapter(adapter);
 	}
@@ -65,7 +83,7 @@ public class TimelineActivity extends Activity {
 	@Override
 	public void onDestroy() {
 		super.onDestroy();
-		db.close();
+		yamba.getStatusData().close();
 	}
 
 }
